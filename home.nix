faa01217094c68f0ef35204cfc5c0e2961fcaee4
yaku <delta.dxuser@gmail.com>
2026-02-06 19:50:37 +0700

    update

--- home.nix
+++ home.nix
@@ -72,7 +72,14 @@
 	home.packages = with pkgs; [
 	  ripgrep
 	  brave
-	  nix-search-tv
+	  pkgs.writeShellApplication {
+	      name = "ns";
+	      runtimeInputs = with pkgs; [
+	        fzf
+	        nix-search-tv
+	      ];
+	      text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+	    }
 	  fishPlugins.tide
 	];
 
